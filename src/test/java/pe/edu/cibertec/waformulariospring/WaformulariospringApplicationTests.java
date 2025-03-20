a0742e331c8ffe05239378ce36d1d1983b4de4be
cunceci <cornelioen.roldan@gmail.com>
2025-03-20 09:40:39 -0500

    Diseño de login terminado

---
+++ src/test/java/pe/edu/cibertec/waformulariospring/WaformulariospringApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.waformulariospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaformulariospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
