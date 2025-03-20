07a768547979db535f0d637517c0e12f82871aca
cunceci <cornelioen.roldan@gmail.com>
2025-03-20 15:16:24 -0500

    Login controller y diseño terminado

--- src/main/java/pe/edu/cibertec/waformulariospring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariospring/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.edu.cibertec.waformulariospring.model.LoginModel;
 
 @Controller
@@ -11,8 +13,17 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login(Model model) {
-        model.addAttribute("loginModel", new LoginModel());git add .
+        model.addAttribute("loginModel", new LoginModel());
         return "login";
     }
 
+    @PostMapping("/login")
+    public String validarLogin(@ModelAttribute("loginModel") LoginModel loginModel, Model model) {
+        if (loginModel.getUsuario().equals("lsalvatierra") && loginModel.getPassword().equals("123"))
+        {
+            return "home";
+        }
+
+        return "login";
+    }
 }
