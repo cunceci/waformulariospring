1854a86b7f9ed2ba0572bc3cb156febc4e919155
cunceci <cornelioen.roldan@gmail.com>
2025-03-20 10:14:02 -0500

    Modelo agregado

--- src/main/java/pe/edu/cibertec/waformulariospring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariospring/controller/LoginController.java
@@ -1,14 +1,17 @@
 package pe.edu.cibertec.waformulariospring.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pe.edu.cibertec.waformulariospring.model.LoginModel;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login" +
-            "")
-    public String login() {
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginModel", new LoginModel());git add .
         return "login";
     }
 
--- src/main/java/pe/edu/cibertec/waformulariospring/model/LoginModel.java
+++ src/main/java/pe/edu/cibertec/waformulariospring/model/LoginModel.java
@@ -1,4 +1,13 @@
 package pe.edu.cibertec.waformulariospring.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginModel {
+
+    private String usuario;
+    private String password;
+
 }
